c0261cad36f3a280e6406ece4028da483707b2e2
wagner <wwwagnerfs@gmail.com>
2024-03-14 02:44:32 -0400

    mês e ano

--- core/urls.py
+++ core/urls.py
@@ -4,16 +4,25 @@ from core import views
 
 urlpatterns = [
     path('', views.HomeTemplateView.as_view(), name='home'),
-    path('relatorios/pacientes', views.PacientesListView.as_view(),
-         name='relat_pacientes'),
-    
-    path('relatorios/pdfpacientes', views.RelatPdfPacientes.as_view(),
-         name='pdf_pacientes'),
-    
+    path('relatorios/pacientes', views.PacientesListView.as_view(), name='relat_pacientes'),
+    path('relatorios/pdfpacientes', views.RelatPdfPacientes.as_view(), name='pdf_pacientes'),
+
     path('relatorios/pdfpacientesporconv', views.RelatPdfPacientesConvenio.as_view(),
          name='pdf_pacientes_por_convenio'),
-    
     path('relatorios/pacientesporconv', views.RelatPacientesConvenio.as_view(),
          name='pacientes_por_convenio'),
+
+    path('graficos/consultasconvenio', views.ConsConvView.as_view(), name='graf_cons_conv'),
     
+    path('relatorios/consporespecialidade',views.ConsultasEspecialidadeMesListView.as_view(),
+         name='rel_por_espec'),
+    
+    path('relatorios/consporespecialidadepdf',views.RelatConsultasEspecialidadeMes.as_view(),
+         name='rel_cons_por_espec_pdf'),
+    
+    path('relatorios/quantpacientporespec', views.PacientesAtendidosEspecialidadeMesListView.as_view(),
+         name='quant_cons_por_espec'),
+    
+    path('relatorios/quantpacientporespecpdf', views.RelatPacientesAtendidosEspecialidadeMes.as_view(),
+         name='quant_cons_por_espec_pdf'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
+import base64
+from matplotlib import pyplot
 from io import BytesIO
 
 from django.http import HttpResponse
@@ -7,8 +9,11 @@ from django.views import View
 from django.views.generic import TemplateView, ListView
 from xhtml2pdf import pisa
 from django.db.models import Model
+from django.db.models import Count
+from django.db.models.functions import ExtractYear
 
-from core.models import Paciente, Medico, Possui
+
+from core.models import Paciente, Medico, Possui, Convenio, Consulta
 
 
 class HomeTemplateView(TemplateView):
@@ -41,33 +46,30 @@ class RelatPdfPacientes(View):
 
 
 class RelatPdfPacientesConvenio(View):
-    template_name = 'relatorios/pdfpacientesporconv.html'
-    model = Possui
-    context_object_name = 'pppc'
 
     def get(self, request):
-        try:
-            objects = self.model.objects.all()
-            my_dict = {}
-            for paconv in objects:
-                if paconv.convenio.nome not in my_dict:
-                    my_dict[paconv.convenio.nome] = {}
-                my_dict[paconv.convenio.nome][paconv.paciente.nome] = paconv.paciente.idade
+        objects = Possui.objects.all()
+        ppc = {}
+        for paconv in objects:
+            if paconv.convenio.nome not in ppc:
+                ppc[paconv.convenio.nome] = {'convenio': paconv.convenio.nome, 'pacientes': []}
+            ppc[paconv.convenio.nome]['pacientes'].append(paconv.paciente)
 
-            print(my_dict)  
-            data = {self.context_object_name: my_dict}
-            template = get_template(self.template_name)
-            html = template.render(data)
-            result = BytesIO()
-            pisa.pisaDocument(BytesIO(html.encode('utf-8')), result)
-            return HttpResponse(result.getvalue(), content_type='application/pdf')
+        context = {'ppc': ppc.values()}
+        template = get_template('relatorios/pdfpacientesporconv.html')
+        html = template.render(context)
+
+        result = BytesIO()
+        try:
+            pdf = pisa.pisaDocument(BytesIO(html.encode('UTF-8')), result)
+            if not pdf.err:
+                return HttpResponse(result.getvalue(), content_type='application/pdf')
         except Exception as e:
             print(e)
-            return HttpResponse("Ocorreu um erro ao gerar o PDF. Por favor, entre em contato com o administrador.")
-
-
-
-
+        
+        return HttpResponse('Erro ao gerar PDF', content_type='text/plain')
+        
+    
 class RelatPacientesConvenio(View):
     template_name = 'relatorios/pacientesporconv.html'
     model = Possui
@@ -84,4 +86,95 @@ class RelatPacientesConvenio(View):
         return render(request, self.template_name, data)
 
 
+'''
+parte dos gráficos
+'''
+class ConsConvView(TemplateView):
+    template_name = 'graficos/consultasconvenio.html'
 
+    def _criar_grafico(self):
+        convenios = Convenio.objects.all()
+        glabels = []
+        gvalores = []
+        for c in convenios:
+            if Consulta.objects.filter(convenio=c).count() > 0:
+                glabels.append(c.nome)
+                quant = Consulta.objects.filter(convenio=c).count()
+                gvalores.append(quant)
+        pyplot.pie(gvalores, labels=glabels)
+        buffer = BytesIO()
+        pyplot.savefig(buffer, format='png')
+        buffer.seek(0)
+        imagem = buffer.getvalue()
+        grafico = base64.b64encode(imagem)
+        grafico = grafico.decode('utf-8')
+        buffer.close()
+        return grafico
+
+
+
+
+    def get_context_data(self, **kwargs):
+        contexto = super().get_context_data(**kwargs)
+        tabela = Consulta.objects.all().order_by('convenio')
+        contexto['tabela'] = tabela
+        contexto['grafico'] = self._criar_grafico()
+        return contexto
+    
+'''
+'''
+class ConsultasEspecialidadeMesListView(ListView):
+    template_name = 'relatorios/consporespecialidade.html'
+    context_object_name = 'consultas'
+
+    def get_queryset(self):
+        return Consulta.objects.values('medico__especialidade', 'data', 'data__month')\
+            .annotate(num_consultas=Count('id'))
+
+class PacientesAtendidosEspecialidadeMesListView(ListView):
+    template_name = 'relatorios/quantpacientporespec.html'
+    context_object_name = 'pacientes_atendidos'
+
+    def get_queryset(self):
+        return Consulta.objects.values('medico__especialidade', 'data', ano=ExtractYear('data'))\
+            .annotate(num_pacientes=Count('paciente', distinct=True))
+
+class RelatConsultasEspecialidadeMes(View):
+
+    def get(self, request):
+        consultas = Consulta.objects.values('medico__especialidade', 'data__month')\
+            .annotate(num_consultas=Count('id'))
+
+        context = {'consultas': consultas}
+        template = get_template('relatorios/consporespecialidadepdf.html')
+        html = template.render(context)
+
+        result = BytesIO()
+        try:
+            pdf = pisa.pisaDocument(BytesIO(html.encode('UTF-8')), result)
+            if not pdf.err:
+                return HttpResponse(result.getvalue(), content_type='application/pdf')
+        except Exception as e:
+            print(e)
+        
+        return HttpResponse('Erro ao gerar PDF', content_type='text/plain')
+
+class RelatPacientesAtendidosEspecialidadeMes(View):
+
+    def get(self, request):
+        pacientes_atendidos = Consulta.objects.values('medico__especialidade', ano=ExtractYear('data'))\
+            .annotate(num_pacientes=Count('paciente', distinct=True))
+
+        context = {'pacientes_atendidos': pacientes_atendidos}
+        template = get_template('relatorios/quantpacientporespecpdf.html')
+        html = template.render(context)
+
+        result = BytesIO()
+        try:
+            pdf = pisa.pisaDocument(BytesIO(html.encode('UTF-8')), result)
+            if not pdf.err:
+                return HttpResponse(result.getvalue(), content_type='application/pdf')
+        except Exception as e:
+            print(e)
+        
+        return HttpResponse('Erro ao gerar PDF', content_type='text/plain')
